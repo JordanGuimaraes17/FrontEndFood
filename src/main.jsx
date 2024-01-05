9167f23ba54077a07b61420bcd4e995950c4bb23
Jordan Guimarães <jordanggomes@gmail.com>
2024-01-05 09:33:45 -0300

    Iniciando estrutura

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from './styles/theme'
+import GlobalStyle from './styles/global.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>
 )
