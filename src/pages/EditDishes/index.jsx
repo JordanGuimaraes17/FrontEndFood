497e7ed19ddade66809eb03c869ccb83477e5df1
Jordan Guimarães <jordanggomes@gmail.com>
2024-01-29 17:59:33 -0300

    integracao-front-end-e-back-end

--- src/pages/EditDishes/index.jsx
+++ src/pages/EditDishes/index.jsx
@@ -10,15 +10,25 @@ import { Button } from '../../components/Button'
 import { Header02 } from '../../components/Header02'
 import { useNavigate } from 'react-router-dom'
 import { api } from '../../services/api'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export function EditDishes() {
+  const params = useParams()
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [price, setPrice] = useState('')
   const [ingredients, setIngredients] = useState([])
   const [newIngredient, setNewIngredient] = useState('')
-  const [file, setFile] = useState(null)
+  const [avataFile, setAvatarFile] = useState(null)
+  const [avatar, setAvatar] = useState(null)
+  const [dishData, setDishData] = useState({
+    name: '',
+    description: '',
+    ingredients: '',
+    price: '',
+    category: ''
+  })
   const navigate = useNavigate()
   const handleNavegacao = rota => {
     navigate(rota)
@@ -27,56 +37,28 @@ export function EditDishes() {
     setIngredients(prevState => [...prevState, newIngredient])
     setNewIngredient('')
   }
-  function handleImage(event) {
-    const file = event.target.files[0]
-    setFile(file)
-  }
   function handleRemoveIngredient(deleted) {
     setIngredients(prevState =>
       prevState.filter(ingredient => ingredient !== deleted)
     )
   }
-  async function handleNewDish() {
-    if (!name) {
-      return alert('Type the name')
-    }
-    if (newIngredient) {
-      return alert(
-        'You left one ingredient typed but without adding it. Please click on add or erase it.'
-      )
-    }
-    if (!description) {
-      return alert('Type something about it.')
-    }
-    if (!price) {
-      return alert('Fill in the price')
-    }
-    if (!file) {
-      return alert('Please upload/choose one image.')
-    }
-    if (ingredients.length == 0) {
-      return alert('Please add at least one ingredient.')
-    }
-    const formData = new FormData()
-    formData.append('name', name)
-    formData.append('description', description)
-    formData.append('price', price)
-    formData.append('ingredients', ingredients)
-    formData.append('image', file)
-    await api
-      .post('/dishes', formData)
-      .then(() => {
-        alert('Item successfully registered')
-        navigate('/')
-      })
-      .catch(error => {
-        if (error.response) {
-          alert(error.response.data.message)
-        } else {
-          alert('Unable to register')
-        }
-      })
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0]
+    setAvatarFile(file)
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
   }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get(`/dishes/${params.id}`)
+        setDishData(response.data)
+      } catch (error) {
+        console.error('Erro ao obter dados dos pratos', error)
+      }
+    }
+    fetchData()
+  }, [params.id])
 
   return (
     <Container>
@@ -106,7 +88,7 @@ export function EditDishes() {
                         placeholder="Imagem do prato"
                         type="file"
                         accept=".jpg, .jpeg, .png"
-                        onChange={event => handleImage(event)}
+                        onChange={handleChangeAvatar}
                       />
                     </label>
                   </div>
@@ -116,7 +98,7 @@ export function EditDishes() {
                   <Input
                     placeholder="Ex: Salada Ceasar"
                     type="text"
-                    onChange={e => setName(e.target.value)}
+                    value={dishData.name}
                   />
                 </div>
                 <div className="input-wrapper">
@@ -136,18 +118,24 @@ export function EditDishes() {
                     {ingredients.map((ingredient, index) => (
                       <DishesItem
                         key={String(index)}
-                        value={ingredient}
                         onClick={() => {
                           handleRemoveIngredient(ingredient)
                         }}
                       />
                     ))}
-                    <DishesItem
-                      isNew
-                      placeholder="Adicionar"
-                      onChange={e => setNewIngredient(e.target.value)}
-                      onClick={handleAddIngredient}
-                    />
+                    {/* Dividindo a string de ingredientes em um array */}
+                    {dishData.ingredients
+                      .split(', ')
+                      .map((ingredient, index) => (
+                        <DishesItem
+                          key={index}
+                          title={ingredient}
+                          isNew
+                          placeholder={ingredient}
+                          onChange={e => setNewIngredient(e.target.value)}
+                          onClick={handleAddIngredient}
+                        />
+                      ))}
                   </div>
                 </div>
 
@@ -156,7 +144,7 @@ export function EditDishes() {
                   <Input
                     style={{ width: '100%' }}
                     className="price"
-                    placeholder="R$ 00,00"
+                    value={dishData.price}
                     type="number"
                     onChange={e => setPrice(e.target.value)}
                   />
@@ -165,17 +153,10 @@ export function EditDishes() {
             </div>
 
             <p className="label">Descrição</p>
-            <TextArea
-              placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
-              onChange={e => setDescription(e.target.value)}
-            />
+            <TextArea placeholder={dishData.description} />
             <footer>
               <Button title="Excluir prato" />
-              <Button
-                className="button"
-                title="Salvar alterações"
-                onClick={e => handleNewDish(e)}
-              />
+              <Button className="button" title="Salvar alterações" />
             </footer>
           </form>
         </main>
